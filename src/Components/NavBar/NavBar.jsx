09087400b4d63b7f8ed24bc1f65bd8c60793db03
code-with-jon <collegepulsemag@gmail.com>
2020-03-20 05:06:33 -0700

    added default avatar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {useSelector} from 'react-redux';
 import GoogleAuth from '../GoogleAuth/GoogleAuth';
 import Logo from '../../TalkCovidLogo.png'
-
+import DefaultAvatar from '../../avatar-default.jpg'
 export default function NavBar() {
    
    const avatar = useSelector(state => state.firebase.profile.avatarUrl)
@@ -14,7 +14,7 @@ export default function NavBar() {
             <img src={avatar && avatar} alt="avatar" />
             <img src="https://cdn.pixabay.com/photo/2017/09/11/03/18/virus-icon-2737712_960_720.png" height="50px" />
             <img src={Logo} />
-            <img src={null} alt="avatar" />
+            <img src={DefaultAvatar} alt="avatar" />
             <div></div>
          </div>
          <GoogleAuth />
