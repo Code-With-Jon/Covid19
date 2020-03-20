0c2f1d03c065725f37efd1dd46e7b5ee12b5c741
SpiffyTheSpaceman <jameskim056@gmail.com>
2020-03-20 01:16:12 -0700

    Set up basic oauth and getting the profile image thing

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import YoutubeList from './Components/YoutubeList';
 function App() {
   return (
     <div>
-    <div style={{height: '100%', width: '100%'}}>
-      <IframeComponent src="https://www.arcgis.com/apps/Embed/index.html?webmap=14aa9e5660cf42b5b4b546dec6ceec7c" height="100%" width="100%"/>
-      <IframeComponent src="//datawrapper.dwcdn.net/dlpln/1/" height="100%" width="100%"/>
       <NavBar />
+      <div style={{height: '100%', width: '100%'}}>
+        <IframeComponent src="https://www.arcgis.com/apps/Embed/index.html?webmap=14aa9e5660cf42b5b4b546dec6ceec7c" height="100%" width="100%"/>
+        <IframeComponent src="//datawrapper.dwcdn.net/dlpln/1/" height="100%" width="100%"/>
+      </div>
+      <div>
+        <YoutubeList />
+      </div>
     </div>
-<div>
-<YoutubeList />
-</div>
-</div>
   );
 }
 
