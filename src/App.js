c05753842beddaa4eb66277829a30373b64bce70
code-with-jon <collegepulsemag@gmail.com>
2020-03-20 00:18:23 -0700

    added youtube query stream

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import IframeComponent from './Components/Map'
+import YoutubeList from './Components/YoutubeList';
 
 function App() {
   return (
+    <div>
     <div style={{height: '100%', width: '100%'}}>
     <IframeComponent src="https://www.arcgis.com/apps/Embed/index.html?webmap=14aa9e5660cf42b5b4b546dec6ceec7c" height="100%" width="100%"/>
     <IframeComponent src="//datawrapper.dwcdn.net/dlpln/1/" height="100%" width="100%"/>
     </div>
+<div>
+<YoutubeList />
+</div>
+</div>
   );
 }
 
---
+++ src/Components/YoutubeList/index.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import _ from 'lodash';
+
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyCT5YNj0WpEUrt_4K8b3GZ6NoBZTOImXMA';
+
+export default class YoutubeList extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        videos: [],
+        selectedVideo: null
+      };
+  
+      this.videoSearch('nbc');
+    }
+  
+    videoSearch(term) {
+      YTSearch({ key: API_KEY, term: term }, videos => {
+        this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+        }); //Same as this.setState({ videos : videos })
+      });
+    }
+  
+    render() {
+      const videoSearch = _.debounce(term => {
+        this.videoSearch(term);
+      }, 300);
+  
+      return (
+        <div>
+          <h5>Youtube Search:</h5><SearchBar onSearchTermChange={videoSearch} />
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList
+            onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+            videos={this.state.videos}
+          />
+        </div>
+      );
+    }
+  }
