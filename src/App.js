e29e78aa12f53cc912f890d77203c09e01f4e5d2
code-with-jon <collegepulsemag@gmail.com>
2020-03-19 23:46:37 -0700

    added data table

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {
   return (
     <div style={{height: '100%', width: '100%'}}>
     <IframeComponent src="https://www.arcgis.com/apps/Embed/index.html?webmap=14aa9e5660cf42b5b4b546dec6ceec7c" height="100%" width="100%"/>
+    <IframeComponent src="//datawrapper.dwcdn.net/dlpln/1/" height="100%" width="100%"/>
     </div>
   );
 }
+
 
 export default App;
