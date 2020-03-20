4188dd91766363caf184fd4c9171cf0f2f0960fb
code-with-jon <collegepulsemag@gmail.com>
2020-03-20 04:50:06 -0700

    added logo

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import GoogleAuth from '../GoogleAuth/GoogleAuth';
-
+import Logo from '../../TalkCovidLogo.png'
 
 export default function NavBar() {
    
@@ -10,6 +10,8 @@ export default function NavBar() {
    return (
       <div>
          <div>
+            <img src="https://cdn.pixabay.com/photo/2017/09/11/03/18/virus-icon-2737712_960_720.png" height="50px" />
+            <img src={Logo} />
             <img src={null} alt="avatar" />
             <div></div>
          </div>
