ddb9737020291cb1a70456e01126452680665c8f
SpiffyTheSpaceman <jameskim056@gmail.com>
2020-03-20 05:06:34 -0700

    delete google auth component

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
-import GoogleAuth from '../GoogleAuth/GoogleAuth';
+import {useSelector, useDispatch} from 'react-redux';
+import {signInGmail, signOut} from '../../redux/actions/authActions';
 import Logo from '../../TalkCovidLogo.png'
 
 export default function NavBar() {
-   
+
+   const dispatch = useDispatch();
    const avatar = useSelector(state => state.firebase.profile.avatarUrl)
+
+   function loginWithGoogle() {
+      dispatch(signInGmail())
+    }
+
+    function handleSignOut() {
+       dispatch(signOut())
+      }
+      
 
 
    return (
       <div>
          <div>
-            <img src={avatar && avatar} alt="avatar" />
-            <img src="https://cdn.pixabay.com/photo/2017/09/11/03/18/virus-icon-2737712_960_720.png" height="50px" />
+            <img src="https://cdn.pixabay.com/photo/2017/09/11/03/18/virus-icon-2737712_960_720.png" height="50px" alt="Logo"/>
             <img src={Logo} />
-            <img src={null} alt="avatar" />
-            <div></div>
+            <img src={avatar && avatar} alt="avatar" />
+            <button onClick={() => loginWithGoogle()}>Google Signin</button>
+            <button onClick={() => handleSignOut()}>Google Signout</button>
          </div>
-         <GoogleAuth />
       </div>
    )
 
