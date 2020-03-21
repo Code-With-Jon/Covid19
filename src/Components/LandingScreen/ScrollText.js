99978b5bc14757c76e57485476dc782c389f2b4d
code-with-jon <collegepulsemag@gmail.com>
2020-03-20 17:12:21 -0700

    added new scrolling text

--- src/Components/LandingScreen/ScrollText.js
+++ src/Components/LandingScreen/ScrollText.js
@@ -1,15 +1,35 @@
 import React from 'react'
 import Ticker from 'react-ticker'
  
-const ScrollText = () => (
+function ScrollText() {
+
+  function rand(min, max) {
+    var offset = min
+    var range = (max - min) + 1
+    var randomNumber = Math.floor(Math.random() * range) + offset
+    return randomNumber
+  }
+return(
     <Ticker>
         {({ index }) => (
-            <>
-                <h1>For more coronavirus information, call the CO HELP line at 303-389-1687 or 1-877-462-2911</h1>
-                <img src="https://www.trackcorona.live/static/corona/images/svg/doctor.svg" alt="" height='50px' />
-            </>
+            <div style={{display: 'flex', flexDirection: 'row'}}>
+              <img src="https://www.trackcorona.live/static/corona/images/svg/doctor.svg" alt="" height='50px' />
+              {/* <div> */}
+            <h1 style={{marginRight: '10vw'}}>{quotes[rand(0, quotes.length - 1)]}</h1>
+            {/* </div> */}
+            {/* <div style={{padding: '5px'}}> */}
+                
+                {/* </div> */}
+            </div>
         )}
     </Ticker>
 )
- 
+}
 export default ScrollText
+
+
+const quotes = [
+  'CALIFORNIA DECLARED STAY IN PLACE -',
+  'ILLINOIS DECLARED STAY AT HOME -',
+  'NEW YORK DECLARED STAY IN PLACE - ',
+]
