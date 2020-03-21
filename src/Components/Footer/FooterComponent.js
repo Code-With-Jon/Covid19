ee47931dc0b5fe4f9a275b1dc53d04bb8c57162f
code-with-jon <collegepulsemag@gmail.com>
2020-03-20 17:00:08 -0700

    fixed footer

--- src/Components/Footer/FooterComponent.js
+++ src/Components/Footer/FooterComponent.js
@@ -10,7 +10,7 @@ render(){
       <Footer
         columns={[
           {
-            title: 'Helpful Links',
+            title: 'HELPFUL LINKS',
             items: [
               {
                 title: 'WHO',
