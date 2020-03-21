b08a769371f383d7326486024126983e18ea4347
code-with-jon <collegepulsemag@gmail.com>
2020-03-21 14:22:13 -0700

    fixed image uploading

--- src/Pages/CreatePost/CreatePost.jsx
+++ src/Pages/CreatePost/CreatePost.jsx
@@ -17,6 +17,7 @@ export default function CreatePost(props) {
    const [htmlString, setHtmlString] = useState('');
    const [editing, setEditing] = useState(true);
    const [postId, setPostId] = useState('');
+   const [uploadedImages, setUploadedImages] = useState([])
 
    // const onDrop = picture => {
    //   setPictures(picture);
@@ -39,6 +40,33 @@ export default function CreatePost(props) {
       setEditorState(EditorStateObject);
    }
 
+   const uploadCallback = (file) => {
+       // long story short, every time we upload an image, we
+    // need to save it to the state so we can get it's data
+    // later when we decide what to do with it.
+    
+   // Make sure you have a uploadImages: [] as your default state
+    let uploadedImages2 = uploadedImages;
+
+    const imageObject = {
+      file: file,
+      localSrc: URL.createObjectURL(file),
+    }
+
+    uploadedImages2.push(imageObject);
+
+    setUploadedImages( uploadedImages2)
+    
+    // We need to return a promise with the image src
+    // the img src we will use here will be what's needed
+    // to preview it in the browser. This will be different than what
+    // we will see in the index.md file we generate.
+    return new Promise(
+      (resolve, reject) => {
+        resolve({ data: { link: imageObject.localSrc } });
+      }
+    );
+  };
 
    return (
       <div>
@@ -69,7 +97,7 @@ export default function CreatePost(props) {
                   urlEnabled: true,
                   uploadEnabled: true,
                   alignmentEnabled: true,
-                  uploadCallback: true,
+                  uploadCallback: uploadCallback,
                   previewImage: true,
                   inputAccept: 'image/gif,image/jpeg,image/jpg,image/png,image/svg',
                   alt: { present: false, mandatory: false },
