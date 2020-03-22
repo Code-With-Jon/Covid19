0b7b1863a6ccf26ef4c280eb1e4c91e766ebdc78
code-with-jon <collegepulsemag@gmail.com>
2020-03-21 22:50:06 -0700

    added metrics above footer

--- src/Pages/LandingScreen/LandingScreen.jsx
+++ src/Pages/LandingScreen/LandingScreen.jsx
@@ -41,19 +41,19 @@ var url = 'http://newsapi.org/v2/top-headlines?' +
 }
 
 
-
 return(
  <div>
      <ScrollText />
-     <div style={{display: 'flex', flexDirection: 'row'}}>
+     {/* <div style={{display: 'flex', flexDirection: 'row'}}>
 <div style={{display: 'flex', flexDirection: 'column', width: '70vw'}}>
         <IframeComponent src="//datawrapper.dwcdn.net/dlpln/1/" height="100%" width="100%"/>
-        </div>
-        <div style={{display: 'flex', flexDirection: 'column', width: '30vw'}}>
+        </div> */}
+       
+        {/* <div style={{display: 'flex', flexDirection: 'column', width: '30vw'}}>
         <IframeComponent src="https://www.arcgis.com/apps/Embed/index.html?webmap=14aa9e5660cf42b5b4b546dec6ceec7c" height="100%" width="100%"/>
-        </div> 
- </div>
-    <h2 style={{marginLeft: '2vw', paddingLeft: '6vw', marginTop: '2vh', color: 'white', background: 'rgba(0,0,0,.87)', width: '20vw'}}>Latest News</h2>
+        </div>  */}
+
+    <h2 style={{marginLeft: '2vw', paddingLeft: '6vw', marginTop: '0vh', color: 'white', background: 'rgba(0,0,0,.87)', width: '20vw'}}>Latest News</h2>
      <div id='news-articles' style={{display: 'flex', overflow: 'scroll', height: '20vh'}}>
 {newsArticles && newsArticles.map((newsArticle, index) => {
 
@@ -86,8 +86,11 @@ return(
 <div>
 
 <YoutubeList />
-</div>
 
+</div>
+<div style={{display: 'flex', flexDirection: 'column', width: '100vw', height: '60vh'}}>
+        <IframeComponent src="https://www.arcgis.com/apps/opsdashboard/index.html#/85320e2ea5424dfaaa75ae62e5c06e61" height="550vh" width="100%"/>
+        </div>
     </div>
     )
 }
