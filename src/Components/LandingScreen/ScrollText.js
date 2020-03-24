e96a8387bb2f1fab959234f78a8c4e98b80e3bf8
code-with-jon <collegepulsemag@gmail.com>
2020-03-24 04:26:53 -0700

    scroll text nightmare

--- src/Components/LandingScreen/ScrollText.js
+++ src/Components/LandingScreen/ScrollText.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Ticker from 'react-ticker'
- 
+var count;
 function ScrollText() {
 
-  function rand(min, max) {
-    var offset = min
-    var range = (max - min) + 1
-    var randomNumber = Math.floor(Math.random() * range) + offset
-    return randomNumber
+  function rand(count) {
+     count = 0;
+
+    if (count !== 0 && count === quotes.length){
+      count = 0;
+      return count
+    } else count = count++
+    return count
   }
+  
 return(
   <div style={{borderBottom: '1px solid black'}}>
     <Ticker >
@@ -16,7 +20,7 @@ return(
             <div style={{display: 'flex', flexDirection: 'row'}}>
               <img src="https://www.trackcorona.live/static/corona/images/svg/doctor.svg" alt="" height='50px' />
               {/* <div> */}
-            <h1 style={{marginRight: '10vw'}}>{quotes[rand(0, quotes.length - 1)]}</h1>
+            <h1 style={{marginRight: '10vw'}}>{quotes[rand(count)]}</h1>
             {/* </div> */}
             {/* <div style={{padding: '5px'}}> */}
                 
