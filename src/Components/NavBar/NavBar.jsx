3e3085892b7391a7acae3dc09348087a246c6d3d
code-with-jon <collegepulsemag@gmail.com>
2020-03-25 18:36:28 -0700

    burger menu

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -37,12 +37,14 @@ export default function NavBar(props) {
    return (
       <div>
          <div className='menu'>
+         <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
             <Link to='/'>
             <div style={{display: 'flex', flexDirection: 'row', marginLeft: '5vw'}}>
                <img src="https://cdn.pixabay.com/photo/2017/09/11/03/18/virus-icon-2737712_960_720.png" height="50px" alt="Logo" style={{alignSelf: "center"}}/>
                <img src={Logo} style={{height: '20vh', marginTop: '-2vh'}}/>
             </div>
             </Link>
+        
             <div style={{width: '77vw', textAlign: 'end', flexDirection: 'row', display: 'flex', justifyContent: 'flex-end', alignItems: 'center'}}>
          <nav className='nav'>
           <ul style={{display: 'flex', flexDirection: 'row', listStyleType: 'none', color: 'white', paddingRight: '5vw'}}>
@@ -56,8 +58,9 @@ export default function NavBar(props) {
             <HashLink to="/learn/#learn" smooth scroll={scrollToElement}>LEARN</HashLink>
             </li>
           </ul>
+          
         </nav>
-        <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
+        
                     
                         <img src={avatar ? avatar && avatar : DefaultAvatar} alt="avatar" height="80px" style={{borderRadius: 40}}/>
                         {!avatar ? 
